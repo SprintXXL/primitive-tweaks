f9b202ae87c73e15f67c442c31fbc3dbafa10cd9
SprintXXL <mspmain@pm.me>
2026-05-06 19:04:44 +0200

    Add stick drop chance from leaves + make gravel to flint recipe shapeless

--- src/main/java/com/sprintxxl/primitivetweaks/events/BlockDropEvents.java
+++ src/main/java/com/sprintxxl/primitivetweaks/events/BlockDropEvents.java
@@ -3,6 +3,7 @@ package com.sprintxxl.primitivetweaks.events;
 import com.sprintxxl.primitivetweaks.PrimitiveTweaks;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -15,6 +16,11 @@ public class BlockDropEvents {
     {
         if (event.getState().getBlock() == Blocks.GRAVEL) {
             event.getDrops().removeIf(drop -> drop.getItem() == Items.FLINT);
+        }
+        if (event.getState().getBlock() == Blocks.LEAVES || event.getState().getBlock() == Blocks.LEAVES2) {
+            if (Math.random() < 0.10) {
+                event.getDrops().add(new ItemStack(Items.STICK));
+            }
         }
     }
 }
